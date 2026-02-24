515ba837558006eb53d14091e5084f1d6ce6ed91
Dieter Heinz <dieter.heinz@safershorelines.com>
2026-02-24 18:48:34 +0100

    Final Production Fixes

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,11 @@ export default defineConfig({
         rollupOptions: {
             input: {
                 main: resolve(__dirname, 'index.html'),
+                zonen: resolve(__dirname, 'zonen.html'),
+                near_miss: resolve(__dirname, 'near-miss.html'),
+                downloads: resolve(__dirname, 'downloads.html'),
                 register: resolve(__dirname, 'register.html'),
+                impressum: resolve(__dirname, 'impressum.html'),
             },
         },
     },
